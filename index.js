aeaed502216dc610aa9f408fc3cb86c27a73ea55
ezvenfernandes <ezvenf@gmail.com>
2022-07-20 14:59:37 +0530

    Add console middleware

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  //   Actions
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
 app.get("/", (req, res) => {
   res.send({
     message: "Hello World",
